08dd43fa3542c7bbc741c1adcb6ae4e58109c212
= <samrgmtg@gmail.com>
2023-02-22 00:33:16 -0500

    updated scraping tool for gov data (IT)

--- scrapecsv.py
+++ scrapecsv.py
@@ -12,6 +12,7 @@ def process_csvs():
 
     data_path = "./data"
     csvs = load_data(data_path)
+    print(csvs)
     for i in csvs:
         df = pd.read_csv(data_path + "/" + i)
         df.drop(index = range(0 ,6), inplace = True)
@@ -24,26 +25,33 @@ def process_csvs():
 
         company_objects = df.to_dict(orient = 'records')
         all_objects.extend(company_objects)
-        return all_objects
+        print(len(company_objects))
+    return all_objects
 
 def get_database():
-    user = "Samg54"  # NEED TO ADD UR OWN USER
-    passw = ""          # NEED TO ADD UR OWN PASS
+    user = "samg54"  # NEED TO ADD UR OWN USER
+    passw = "mongo999"          # NEED TO ADD UR OWN PASS
+
+    client = MongoClient(f"mongodb+srv://{user}:{passw}@ngcluster.sbambjh.mongodb.net/?retryWrites=true&w=majority&socketTimeoutMS=100000&connectTimeoutMS=100000&serverSelectionTimeoutMS=100000")
 
     # Provide the mongodb atlas url to connect python to mongodb using pymongo
-    CONNECTION_STRING = "mongodb+srv://<" + user + ">:<" + passw + ">@ngcluster.sbambjh.mongodb.net/test"
+    # CONNECTION_STRING = "mongodb+srv://" + user + ":" + passw + "@ngcluster.sbambjh.mongodb.net/test"
     
-    # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
-    client = MongoClient(CONNECTION_STRING)
+    # # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
+    # client = MongoClient(CONNECTION_STRING)
     
     # Create the database for our example (we will use the same database throughout the tutorial
-    return client['Project_0']
+    return client.companiesDB
 
 def main():
     dbname = get_database()
     res = process_csvs()
-    collection_name = dbname['clist']
-    collection_name.insert_one(res[0])
+    collection_name = dbname.govcompanies
+    # collection_name.delete_many({})          # RESET DATABASE IF NEEDED
+    collection_name.insert_many(res)
+    print("Uploaded ", len(res), "documents")
     return res
+
+
  
 main()
