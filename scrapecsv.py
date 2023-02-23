d35e00f6dd155c46c512d61f2826df4474b2d8f7
Aidan Melvin <aidmelvin.314@gmail.com>
2023-02-22 21:44:04 -0500

    a little bit of cleanup

--- scrapecsv.py
+++ scrapecsv.py
@@ -5,7 +5,8 @@ from pymongo import MongoClient
 
 
 def load_data(data_path):
-    return (os.listdir(data_path))
+    return os.listdir(data_path)
+
 
 def process_csvs():
     all_objects = []
@@ -15,18 +16,19 @@ def process_csvs():
     print(csvs)
     for i in csvs:
         df = pd.read_csv(data_path + "/" + i)
-        df.drop(index = range(0 ,6), inplace = True)
+        df.drop(index=range(0, 6), inplace=True)
         df['cname'] = df.Vendor
         df['website'] = df.URL
         df['industry'] = df['Sub Category']
 
         keep_cols = ['cname', 'website', 'industry']
-        df.drop(columns = ([i for i in list(df.columns) if i not in keep_cols]), axis = 1, inplace = True)
+        df.drop(columns=([i for i in list(df.columns) if i not in keep_cols]), axis=1, inplace=True)
 
-        company_objects = df.to_dict(orient = 'records')
+        company_objects = df.to_dict(orient='records')
         all_objects.extend(company_objects)
         print(len(company_objects))
     return all_objects
+
 
 def get_database():
     user = "samg54"  # NEED TO ADD UR OWN USER
@@ -43,6 +45,7 @@ def get_database():
     # Create the database for our example (we will use the same database throughout the tutorial
     return client.companiesDB
 
+
 def main():
     dbname = get_database()
     res = process_csvs()
@@ -53,5 +56,4 @@ def main():
     return res
 
 
- 
 main()
