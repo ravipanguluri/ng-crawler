39ebdcf1a226b0e31be00da5186e505a20959220
Aidan Melvin <aidmelvin.314@gmail.com>
2023-02-24 18:55:28 -0500

    uploaded mongo code not working

--- ScrapeAsyncResult.py
+++ ScrapeAsyncResult.py
@@ -3,16 +3,29 @@ from bs4 import BeautifulSoup
 
 
 class ScrapeAsyncResult:
-    def __init__(self, raw_html: str):
-        self.raw_html: str = raw_html
+    def __init__(self, root_link: str, cname: str, industry: str):
+        self.root_link: str = root_link
+        self.raw_html: str = ''
         self.links = []
+        self.cancelled = False
+        self.cname = cname
+        self.industry = industry
+
+    def set_raw_html(self, raw_html: str):
+        self.raw_html = raw_html
         soup = BeautifulSoup(raw_html, "html.parser")
         for tag in soup.find_all():
             if tag.name == 'a':
-                self.links.append(str(tag['href']))
+                try:
+                    self.links.append(str(tag['href']))
+                except KeyError:
+                    pass
 
     def get_raw_html(self):
-        return self.raw_html
+        if not self.cancelled:
+            return self.raw_html
+        else:
+            return 'n/a'
 
     def get_all_links(self):
         return self.links
--- scrape_test.py
+++
@@ -1,8 +0,0 @@
-
-from bs4 import BeautifulSoup
-import requests
-
-# I am not adding the CSV data to this GH repo, it would make it enormous
-# you can get it from here: https://www.gsaelibrary.gsa.gov/ElibMain/downloadInfo.do?source=MAS&fromPage=ss
-
-
