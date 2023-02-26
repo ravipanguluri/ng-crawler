79c0f98c97c5efb7bfb522933bc1d7189a2e9eef
Aidan Melvin <aidmelvin.314@gmail.com>
2023-02-26 16:48:40 -0500

    data scraping scripts

--- scrape_angellist.py
+++ scrape_angellist.py
@@ -1,8 +1,46 @@
 
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import pandas as pd
+fh = open('./data/html_178_to_200.txt')
 
-driver = webdriver.Chrome('/Users/aidanmelvin/Downloads/chromedriver_mac_arm64/chromedriver')
+csv_writing = open('./data/csv_angel_output.csv', 'a')
 
-driver.get('https://angel.co/startups?page=5')
+html_lines = fh.readlines()
+
+results = []
+
+company_name = ''
+website = ''
+num_employees = ''
+description = ''
+
+for line in html_lines:
+    index = line.find('text-dark-aaaa font-medium antialiased text-lg')
+    company_name = ''
+    website = ''
+    num_employees = ''
+    description = ''
+    if index > -1:
+        # results.append({line[index]})
+        # company name:
+        # print(line[index + 53:line.find('</div>', index + 54)])
+        company_name = line[index + 53:line.find('</div>', index + 54)]
+        # website:
+        website_start_index = line.find('<dt class="mb-2 font-bold">Website</dt>')
+        # print(line[website_start_index+52:line.find('"', website_start_index + 53)])
+        website = line[website_start_index+52:line.find('"', website_start_index + 53)]
+
+        # num employees
+        employees_start = line.find('<dt class="mb-2 font-bold">Employees</dt>')
+        # print(line[employees_start+67:line.find('</dd>', employees_start + 68)])
+        num_employees = line[employees_start+67:line.find('</dd>', employees_start + 68)]
+
+    # brief description
+    description_start = line.find('<div class="styles_component__481pO">')
+    if description_start > -1:
+        # print(line[description_start+42:line.find('</div>', description_start + 43)])
+        description = line[description_start + 42:line.find('</div>', description_start + 43)]
+
+    if company_name != '' and website != '' and num_employees != '' and description != '':
+        csv_writing.write(f'{company_name.replace(",", "")},{website.replace(",", "")},{num_employees.replace(",", "")},{description.replace(",", "")}\n')
+
+csv_writing.close()
+fh.close()
--- scrapecsv.py
+++ scrapecsv.py
@@ -5,28 +5,30 @@ from pymongo import MongoClient
 
 
 def load_data(data_path):
-    return (os.listdir(data_path))
+    return os.listdir(data_path)
 
 def process_csvs():
     all_objects = []
 
-    data_path = "./data"
-    csvs = load_data(data_path)
-    print(csvs)
-    for i in csvs:
-        df = pd.read_csv(data_path + "/" + i)
-        df.drop(index = range(0 ,6), inplace = True)
-        df['cname'] = df.Vendor
-        df['website'] = df.URL
-        df['industry'] = df['Sub Category']
+    data_path = "./data/csv_angel_output.csv"
 
-        keep_cols = ['cname', 'website', 'industry']
-        df.drop(columns = ([i for i in list(df.columns) if i not in keep_cols]), axis = 1, inplace = True)
-
-        company_objects = df.to_dict(orient = 'records')
-        all_objects.extend(company_objects)
-        print(len(company_objects))
-    return all_objects
+    df = pd.read_csv(data_path)
+    # print(df.head(5))
+    # print(df.columns)
+    # df.drop(index = range(0 ,6), inplace = True)
+    # df['cname'] = df.Vendor
+    # df['website'] = df.URL
+    # df['industry'] = df['Sub Category']
+    #
+    # keep_cols = ['cname', 'website', 'industry']
+    # df.drop(columns = ([i for i in list(df.columns) if i not in keep_cols]), axis = 1, inplace = True)
+    #
+    company_objects = df.to_dict(orient = 'records')
+    print(company_objects)
+    # all_objects.extend(company_objects)
+    # print(len(company_objects))
+    #
+    # return all_objects
 
 def get_database():
     user = "samg54"  # NEED TO ADD UR OWN USER
@@ -54,4 +56,5 @@ def main():
 
 
  
-main()
+# main()
+process_csvs()
